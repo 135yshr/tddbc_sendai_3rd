ba621ca00c4e2101fed44ac827097392e0bc1e88
Yoshihiro Isago <isago@praisefirst.jp>
2013-10-23 20:00:54 +0900

    下端点が取得できるテストを実装

--- ClosedRange_test.go
+++ ClosedRange_test.go
@@ -10,3 +10,11 @@ func Test正常_インスタンスが作成できる(t *testing.T) {
 		t.Error("instance is nil")
 	}
 }
+
+func Test正常_閉区間3to8を作成し下端点が取得できることを確認する(t *testing.T) {
+	expected := 3
+	target := NewClosedRange(3, 8)
+	if target.lower != expected {
+		t.Errorf("failed get lower expected=%d, actual=%d\n", expected, target.lower)
+	}
+}
