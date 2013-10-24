a76d74e114196c353a6e59fe3e995019e52414dc
Yoshihiro Isago <isago@praisefirst.jp>
2013-10-24 17:31:40 +0900

    開区間を作成し下端点が取得できることを確認するテストを追加

--- OpenRange_test.go
+++ OpenRange_test.go
@@ -3,3 +3,21 @@ package tddbc
 import (
 	"testing"
 )
+
+func Test正常_開区間を作成し下端点が取得できることを確認する(t *testing.T) {
+	var data = []struct {
+		lower    int
+		upper    int
+		expected int
+	}{
+		{3, 8, 3},
+	}
+
+	for _, d := range data {
+		expected := d.expected
+		target := NewOpenRange(d.lower, d.upper)
+		if target.lower != expected {
+			t.Errorf("failed get lower expected=%d, actual=%d\n", expected, target.lower)
+		}
+	}
+}
