0d79ea0c9e32fb568f8e2a221b837885abd8d397
Yoshihiro Isago <isago@praisefirst.jp>
2013-10-29 08:05:09 +0900

    開区間同士が接続していないパターンを追加

--- OpenRange_test.go
+++ OpenRange_test.go
@@ -121,6 +121,7 @@ func Test正常_開区間が別の開区間と接続しているか確認する(
 		expected bool
 	}{
 		{OpenRange{3, 8}, OpenRange{3, 8}, true},
+		{OpenRange{3, 8}, OpenRange{1, 3}, false},
 	}
 	for _, d := range data {
 		expected := d.expected
